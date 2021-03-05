481e7419665537d0c5060ea25ba472d81aab6d7b
msbike <mail@jens-keull.de>
2021-03-05 07:58:11 +0100

    Added attributes to Model

--- products/models.py
+++ products/models.py
@@ -5,6 +5,6 @@ from django.utils import timezone
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=120)
-    description = models.TextField()
-    price = models.IntegerField
+    description = models.TextField(blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
     valid_from = models.DateField(default=timezone.now)
