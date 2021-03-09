71962cecdc699b64e6ee8cc22d7d93ffbc7be68a
msbike <mail@jens-keull.de>
2021-03-09 06:11:18 +0100

    Add contact page

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home_view(*args, **kwargs):
     return HttpResponse('<h1>Hello Pages</h1>')
+
+
+def contact_view(*args, **kwargs):
+    return HttpResponse('<h1>Contact</h1>')
