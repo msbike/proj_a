dcfa87972a058e71ca08ddd6ac8a7acf1a47ec2d
msbike <mail@jens-keull.de>
2021-03-09 07:44:44 +0100

    Several pages with templates

--- pages/views.py
+++ pages/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 
 
 def home_view(request, *args, **kwargs):
-    # return HttpResponse('<h1>Hello Pages</h1>')
     return render(request, "home.html", {})
 
 
 def contact_view(request, *args, **kwargs):
-    print(request.user)
-    return HttpResponse('<h1>Contact</h1>')
+    return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
