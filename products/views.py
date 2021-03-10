3ae549d6adf79621deed688ea6e4ff0f3a2c056b
msbike <mail@jens-keull.de>
2021-03-10 16:18:39 +0100

    Raw form

--- products/views.py
+++ products/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 
 def product_create_view(request):
 
-    if request.method == "POST":
-        my_new_title = request.POST.get('title')
-        print(my_new_title)
-        # Product.objects.create(title=my_new_title)
+    form = RawProductForm()
+
+    if request.method == 'POST':
+        form = RawProductForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            Product.objects.create(**form.cleaned_data)
+        else:
+            print(form.errors)
 
     context = {
+        "form": form
     }
     return render(request, 'products/product_create.html', context)
+
+# def product_create_view(request):
+#
+#     if request.method == "POST":
+#         my_new_title = request.POST.get('title')
+#         print(my_new_title)
+#         # Product.objects.create(title=my_new_title)
+#
+#     context = {
+#     }
+#     return render(request, 'products/product_create.html', context)
 
 # def product_create_view(request):
 #     form = ProductForm(request.POST or None)
