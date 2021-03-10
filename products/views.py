dc982a6f6d0d454a364c4b2f2da17888057a7473
msbike <mail@jens-keull.de>
2021-03-10 07:31:52 +0100

    Form created

--- products/views.py
+++ products/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
 
 
-def product_detail_view(request, *args, **kwargs):
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'products/product_create.html', context)
+
+
+def product_detail_view(request):
     obj = Product.objects.get(id=2)
     context = {
         'title': obj.title,
         'description': obj.description
-        #'product_list': Product.objects.
     }
     return render(request, 'products/detail.html', context)
